09be7ee980d4a84491d7816befa86b9b1af1517a
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-30 03:10:32 +0600

    second event handler added

--- cruiseQueen.js
+++ cruiseQueen.js
@@ -1,10 +1,38 @@
 document.getElementById("plus-btn").addEventListener("click", function(){
-    const fCountInput = document.getElementById("fClass-count");
-    const newFCountInput = parseFloat(fCountInput.value) + 1;
-    fCountInput.value = newFCountInput;
+    fClassEventHandler(true);
 });
 document.getElementById("minus-btn").addEventListener("click", function(){
+    fClassEventHandler(false);
+});
+function fClassEventHandler(isIncrease){
     const fCountInput = document.getElementById("fClass-count");
-    const newFCountInput = parseFloat(fCountInput.value) - 1;
-    fCountInput.value = newFCountInput;
-})
+    const newFCountInput = parseFloat(fCountInput.value);
+    let fCount = newFCountInput;
+    if(isIncrease == true){
+        fCount = newFCountInput + 1;
+    }
+    if(isIncrease == false && newFCountInput > 0){
+        fCount = newFCountInput - 1;
+    }
+    fCountInput.value = fCount;
+
+}
+
+document.getElementById("ePlus-btn").addEventListener("click", function(){
+    eClassEventHandler(true);
+});
+document.getElementById("eMinus-btn").addEventListener("click", function(){
+    eClassEventHandler(false);
+});
+function eClassEventHandler(isIncrease){
+    const eCountInput = document.getElementById("eClass-count");
+    const newECountInput = parseFloat(eCountInput.value);
+    let eCount = newECountInput;
+    if(isIncrease == true){
+        eCount = newECountInput + 1;
+    }
+    if(isIncrease == false && newECountInput > 0){
+        eCount = newECountInput - 1;
+    }
+    eCountInput.value = eCount;
+}
