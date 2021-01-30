b44435704b4c1b6a5a30c69ea51962d1e48d3275
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-30 11:42:02 +0600

    organised clean code

--- cruiseQueen.js
+++ cruiseQueen.js
@@ -1,42 +1,58 @@
-document.getElementById("plus-btn").addEventListener("click", function(){
-    fClassEventHandler(true);
-});
-document.getElementById("minus-btn").addEventListener("click", function(){
-    fClassEventHandler(false);
-});
-function fClassEventHandler(isIncrease){
-    const fCountInput = document.getElementById("fClass-count");
-    const newFCountInput = parseFloat(fCountInput.value);
-    let fCount = newFCountInput;
+// document.getElementById("plus-btn").addEventListener("click", function(){
+//     fClassEventHandler(true);
+// });
+// document.getElementById("minus-btn").addEventListener("click", function(){
+//     fClassEventHandler(false);
+// });
+// function fClassEventHandler(isIncrease){
+//     const fCountInput = document.getElementById("fClass-count");
+//     const newFCountInput = parseFloat(fCountInput.value);
+//     let fCount = newFCountInput;
+//     if(isIncrease == true){
+//         fCount = newFCountInput + 1;
+//     }
+//     if(isIncrease == false && newFCountInput > 0){
+//         fCount = newFCountInput - 1;
+//     }
+//     fCountInput.value = fCount;
+//     subTotalEvent();
+// }
+
+// document.getElementById("ePlus-btn").addEventListener("click", function(){
+//     eClassEventHandler(true);
+// });
+// document.getElementById("eMinus-btn").addEventListener("click", function(){
+//     eClassEventHandler(false);
+// });
+// function eClassEventHandler(isIncrease){
+//     const eCountInput = document.getElementById("eClass-count");
+//     const newECountInput = parseFloat(eCountInput.value);
+//     let eCount = newECountInput;
+//     if(isIncrease == true){
+//         eCount = newECountInput + 1;
+//     }
+//     if(isIncrease == false && newECountInput > 0){
+//         eCount = newECountInput - 1;
+//     }
+//     eCountInput.value = eCount;
+    
+// }
+
+function classEventHandler(ticket, isIncrease){
+    const ticketInputCount = document.getElementById(ticket + "-count");
+    const newTicketInputCount = parseFloat(ticketInputCount.value);
+    let ticketCount = newTicketInputCount;
     if(isIncrease == true){
-        fCount = newFCountInput + 1;
+        ticketCount = newTicketInputCount +1;
     }
-    if(isIncrease == false && newFCountInput > 0){
-        fCount = newFCountInput - 1;
+    if(isIncrease == false && newTicketInputCount > 0){
+        ticketCount = newTicketInputCount -1;
     }
-    fCountInput.value = fCount;
+    ticketInputCount.value = ticketCount;
     subTotalEvent();
 }
 
-document.getElementById("ePlus-btn").addEventListener("click", function(){
-    eClassEventHandler(true);
-});
-document.getElementById("eMinus-btn").addEventListener("click", function(){
-    eClassEventHandler(false);
-});
-function eClassEventHandler(isIncrease){
-    const eCountInput = document.getElementById("eClass-count");
-    const newECountInput = parseFloat(eCountInput.value);
-    let eCount = newECountInput;
-    if(isIncrease == true){
-        eCount = newECountInput + 1;
-    }
-    if(isIncrease == false && newECountInput > 0){
-        eCount = newECountInput - 1;
-    }
-    eCountInput.value = eCount;
-    subTotalEvent();
-}
+
 function subTotalEvent(){
     const fClassPrice = document.getElementById("fClass-count");
     const newFClassPrice = parseFloat(fClassPrice.value);
