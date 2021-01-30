2939e30a30e95b324bbecb6c25804025ec175d6f
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-30 12:21:47 +0600

    added all calculation

--- cruiseQueen.js
+++ cruiseQueen.js
@@ -1,3 +1,39 @@
+
+// organized my working code 
+
+function classEventHandler(ticket, isIncrease){
+    const ticketInputCount = document.getElementById(ticket + "-count");
+    const newTicketInputCount = parseFloat(ticketInputCount.value);
+    let ticketCount = newTicketInputCount;
+    if(isIncrease == true){
+        ticketCount = newTicketInputCount +1;
+    }
+    if(isIncrease == false && newTicketInputCount > 0){
+        ticketCount = newTicketInputCount -1;
+    }
+    ticketInputCount.value = ticketCount;
+    calculateValue();
+}
+
+function calculateValue(){
+    const fClassCount = getInputValue("fClass");
+    const eClassCount = getInputValue("eClass");
+    const fCount = fClassCount*150 + eClassCount*100;
+    document.getElementById("sub-total").innerText = fCount;
+    const tax = fCount * 0.1;
+    document.getElementById("tax-total").innerText = tax;
+    const grandTotal = tax + fCount;
+    document.getElementById("grand-total").innerText = grandTotal;
+}
+
+function getInputValue(ticket){
+    const ticketInput = document.getElementById(ticket + "-count");
+    const newTicketInput = parseFloat(ticketInput.value);
+    return newTicketInput;
+}
+
+
+// my working code
 // document.getElementById("plus-btn").addEventListener("click", function(){
 //     fClassEventHandler(true);
 // });
@@ -38,26 +74,11 @@
     
 // }
 
-function classEventHandler(ticket, isIncrease){
-    const ticketInputCount = document.getElementById(ticket + "-count");
-    const newTicketInputCount = parseFloat(ticketInputCount.value);
-    let ticketCount = newTicketInputCount;
-    if(isIncrease == true){
-        ticketCount = newTicketInputCount +1;
-    }
-    if(isIncrease == false && newTicketInputCount > 0){
-        ticketCount = newTicketInputCount -1;
-    }
-    ticketInputCount.value = ticketCount;
-    subTotalEvent();
-}
-
-
-function subTotalEvent(){
-    const fClassPrice = document.getElementById("fClass-count");
-    const newFClassPrice = parseFloat(fClassPrice.value);
-    const eClassPrice = document.getElementById("eClass-count");
-    const newEClassPrice = parseFloat(eClassPrice.value);
-    subTotalPrice = newFClassPrice * 150 + newEClassPrice * 100;
-    document.getElementById("sub-total").innerText = subTotalPrice;
-}
+// function subTotalEvent(){
+//     const fClassPrice = document.getElementById("fClass-count");
+//     const newFClassPrice = parseFloat(fClassPrice.value);
+//     const eClassPrice = document.getElementById("eClass-count");
+//     const newEClassPrice = parseFloat(eClassPrice.value);
+//     subTotalPrice = newFClassPrice * 150 + newEClassPrice * 100;
+//     document.getElementById("sub-total").innerText = subTotalPrice;
+// }
